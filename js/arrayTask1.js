dd30a3a49bb54e5a0cd0911969eb893d2996d7b0
SergeiOleynik <154737237+SergeiOleynik@users.noreply.github.com>
2024-02-09 11:49:10 +0200

    1

--- js/arrayTask1.js
+++ js/arrayTask1.js
@@ -2,11 +2,13 @@ const wordsArray = ["apple", "banana", "orange", "kiwi", "grape"],
       n = 5;
 let   newArray = [];
 
-for(const arr of wordsArray){
-  if(arr.slice("").length>n) newArray.push(arr); 
-}
-console.log(newArray);
+alert(`масив до перетворення : ${wordsArray}`);
 
+for(const arr of wordsArray){
+  if(arr.slice("").length>n) newArray.push(arr);
+} 
+
+alert(`масив, що містить лише ті слова, які мають довжину більше 5 : ${newArray}`);
 
 
 
